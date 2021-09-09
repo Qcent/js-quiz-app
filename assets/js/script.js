d40c9266c2c57e70945956ddca987982acd40524
Dave Quinn <dquinn8@cogeco.ca>
2021-09-09 19:56:15 -0400

    added the endgame screen where score is displayed and an input for Initials

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,9 @@ let startQuizBtn = document.querySelector("#start-quiz");
 let viewScores = document.querySelector("#high-score");
 let timerValue = document.querySelector("#timer-value");
 let introCard = document.querySelector(".intro");
+let endGame = document.createElement("div");
 let qHolder = document.querySelector("main");
+let quizFeedback = document.querySelector("#feedback");
 
 let quizTimer = 240;
 let score = 0;
@@ -21,17 +23,19 @@ const quizQuestion = [{
     }
 ];
 const showHighScore = function() {
-    alert("these are teh highScores");
+    alert("these are the highScores");
 }
 const runQuizTimer = function() {
-    if (stopQuiz) { return; } // abort if stopQuiz is true
+    if (stopQuiz) { // abort if stopQuiz is true
+        return;
+    }
     setTimeout(function() {
         if (stopQuiz) { return; } // abort if stopQuiz is true
         quizTimer--; // decrement timer
         timerValue.textContent = quizTimer; //update timer on screen
         if (quizTimer <= 0) { // if out of time 
             stopQuiz = true; // stop quiz
-            alert("Time's Up"); //alert
+            endQuiz("Time's Up"); //alert
             return; // abort countdown
         }
         runQuizTimer(); // else run this function again 
@@ -79,13 +83,15 @@ const questionButtonHandler = function(event) {
 
         nextQuestion();
     }
+    if (event.target.matches("span.btn")) { //only triggers if a span button is clicked ie the endGame high score form
+        alert("high score entered");
+    }
 };
 const nextQuestion = function() {
     currentQuestion++;
     if (currentQuestion >= quizQuestion.length) {
         stopQuiz = true;
-        alert("You Reached the End!! \nYour Score: " + score + "/" +
-            quizQuestion.length);
+        endQuiz();
         return;
     }
     renderQuestion(currentQuestion);
@@ -107,6 +113,44 @@ const startQuiz = function() {
     //show a question 
     nextQuestion(); //will advance currentQuestion and display it
     //all further steps are handeled byt the qHolder event listener
+
+};
+const renderEndGame = function(msg) {
+    //create div element of class question
+    endGame.className = "outro";
+
+    // add h2 title
+    let title = document.createElement('h2');
+    title.textContent = msg; // out of time!  | all done!
+
+    //create div with score
+    let text = document.createElement("div");
+    text.textContent = "Your final score is " + score + ".";
+
+    //another div for the form
+    let form = document.createElement('div');
+    form.className = "form";
+    form.innerHTML = "<span>Enter Initials:</span> <input type='text' maxlength=4 id='hs-name'><span class='btn highscore'>Submit</span>";
+
+    //append all elements
+    endGame.appendChild(title);
+    endGame.appendChild(text);
+    endGame.appendChild(form);
+
+    qHolder.appendChild(endGame);
+}
+const endQuiz = function(msg) {
+
+    if (!msg) { msg = "All done!"; } // make sure this isnt null
+
+    // qHolder.appendChild(introCard); // restore the start screen
+
+    // calculate score
+    score += quizTimer;
+
+    renderEndGame(msg);
+
+    //quizFeedback.textContent = msg + "Your Score: " + score;
 
 };
 
